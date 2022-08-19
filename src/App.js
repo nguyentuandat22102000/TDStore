9f706b36e92b78b1fde03aa031649b6dcf46ae9b
Đạt Nguyễn <datnguyen@ats-MacBook-Air.local>
2022-08-19 18:05:49 +0700

    lan 2

--- src/App.js
+++ src/App.js
@@ -10,42 +10,11 @@ import Blog from './components/Blog/Blog';
 import Contact from './components/Contact/Contact';
 
 function App() {
-  const futures = [
-    {
-      id:1,
-      img: Shipping,
-      tittle: "Free Shipping",
-      disc: "ON ORDER OVER $50",
-    },
-    {
-      id:2,
-      img: Support,
-      tittle: "SUPPORT",
-      disc: "24/7 SUPPORT",
-    },
-    {
-      id:3,
-      img: Quatity,
-      tittle: "SUPERIOR QUALITY",
-      disc: "QUALITY PRODUCTS",
-    },
-  ];
-  const futureList = futures.map((future) => (
-    <div key={future.id} className="col-span-2 mx-auto text-center cursor-pointer shadow-xl w-[300px] h-[200px] hover:border-indigo-200 hover:border-2 hover:text-red-400">
-      <img src={future.img} alt={future.tittle} className="mx-auto w-[100px] h-[100px]"/>
-      <h2 className="my-4 font-bold">{future.tittle}</h2>
-      <span className="text-gray-400">{future.disc}</span>
-    </div>
-  ));
+  
   return (
     <div className="w-full h-screen lg:relative">
       <Header />
       <Banner />
-      <div className="container__wide grid grid-cols-6 py-10">
-          {
-            futureList
-          }
-      </div>
       <Category/>
       <Product/>
       <Blog/>
